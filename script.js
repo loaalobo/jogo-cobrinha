98677df91b9a024239a650d4f86e988bc8b50e34
Lorena Lobo <lorena.aalobo@gmail.com>
2021-03-11 08:25:17 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //o contexto renderiza o desenho que vai acontecer dentro do canvas
 let box = 32;
+let snake = []; //for
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 //função que desenha o canvas: desenha e define campo
 function criarBG() {
@@ -8,4 +13,17 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo onde acontece o jogo - trabalha com 4 parâmetros
 }
 
+//a cobrinha será um array de coordenadas
+//adicionaremos um elemento e retiraremos o último (isso vai fazer com que ela ande)
+
+function criarCobrinha() {
+    //for percorre todo o tamanho do array e vai incrementar
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+
+    }
+}
+
 criarBG();
+criarCobrinha();
